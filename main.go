2ac543db817e253cce1e49efe6f78f8a050c256d
James Keveren <james@keve.ren>
2022-01-23 00:00:41 +0000

    basic working with tests

--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import ()
+import (
+	"bytes"
+	"golang.org/x/sys/unix"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func main() {
+	l := log.New(os.Stdout, "", 0)                              // log
+	le := log.New(os.Stderr, "find-by-extended-attribute: ", 0) // log error
 
+	// handle bad input
+	if len(os.Args) < 2 {
+		le.Fatalf("Not enough arguments. Usage: find-by-extended-attribute <xattr name> [<xattr value>]")
+	}
+
+	// parse input
+	xattrName := os.Args[1]
+	var xattrValue []byte
+	if len(os.Args) > 2 {
+		xattrValue = []byte(os.Args[2])
+	}
+
+	// start recursion
+	recurse(l, le, xattrName, xattrValue, ".")
+}
+
+func recurse(l, le *log.Logger, xattrName string, xattrValue []byte, p string) {
+	// open file
+	f, err := os.Open(p)
+	if err != nil {
+		le.Println(err)
+		return
+	}
+	defer f.Close()
+
+	// stat file
+	info, err := f.Stat()
+	if err != nil {
+		le.Println(err)
+		return
+	}
+
+	if info.IsDir() {
+		// recurse deeper through directory
+		// read directory
+		names, err := f.Readdirnames(0)
+		if err != nil {
+			le.Println(err)
+			return
+		}
+
+		// iterate through contents
+		for _, name := range names {
+			recurse(l, le, xattrName, xattrValue, filepath.Join(p, name))
+		}
+	} else {
+		// check xattrs of file
+		dest := make([]byte, len(xattrValue)) // len of 0 because most files will likely have no xattr value
+		_, err := unix.Fgetxattr(int(f.Fd()), xattrName, dest)
+		if err == unix.ENODATA {
+
+		} else if err == nil {
+			if bytes.Equal(xattrValue, dest) {
+				l.Println(p)
+			}
+		} else {
+			le.Println(err)
+			return
+		}
+	}
 }
--- main_test.go
+++ main_test.go
@@ -2,27 +2,139 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/sys/unix"
+	"io/fs"
 	"os"
 	"os/exec"
 	"testing"
 )
 
-func TestMain(m *testing.M) {
+func Test(t *testing.T) {
+	// build
 	err := exec.Command("go", "build").Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
-	code := m.Run()
-	os.Remove("find-by-extended-attribute")
-	os.Exit(code)
-}
 
-func Test(t *testing.T) {
+	// remove any existing test files
+	// This ensures there are no incompatable files from executions of previous versions of the test squite.
+	err = os.RemoveAll("test")
+	if err != nil {
+		panic(err)
+	}
+
+	// create test directory
+	err = os.MkdirAll("test", fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	// create files with xattrs
+	files := []struct {
+		name       string
+		xattrName  string
+		xattrValue string
+	}{
+		// using random nouns because they are easy to recognise; makes debugging easier
+		// without value
+		{"gene", "pie", ""},
+		{"artisan", "variety", ""},
+		{"youth", "variety", ""},
+		// with value
+		{"activity", "mum", "climate"},
+		{"event", "health", "security"},
+		{"policy", "health", "security"},
+	}
+	for _, file := range files {
+		// create file
+		f, err := os.Create("test/" + file.name)
+		if err != nil {
+			panic(err)
+		}
+		// set xattr
+		err = unix.Fsetxattr(int(f.Fd()), "user."+file.xattrName, []byte(file.xattrValue), 0)
+		if err != nil {
+			panic(err)
+		}
+		f.Close()
+	}
+
+	t.Cleanup(func() {
+		// cleanup
+		// remove executable
+		err = os.Remove("find-by-extended-attribute")
+		if err != nil {
+			panic(err)
+		}
+	})
+
 	t.Run("symlink", func(t *testing.T) {
-		err := exec.Command("./fbea").Run()
+		got, err := os.Readlink("fbea")
 		if err != nil {
 			t.Fatal(err)
 		}
+		want := "find-by-extended-attribute"
+		if got != want {
+			t.Fatalf("Want: %s, Got %s", want, got)
+		}
+	})
+
+	t.Run("stdout", func(t *testing.T) {
+		t.Run("justXattrName", func(t *testing.T) {
+			t.Run("single", func(t *testing.T) {
+				out, err := exec.Command("./find-by-extended-attribute", "user.pie").CombinedOutput()
+				if err != nil {
+					t.Log(string(out))
+					t.Fatal(err)
+				}
+				got := string(out)
+				want := "test/gene\n"
+				if got != want {
+					t.Fatalf("Want: %s, Got: %s", want, got)
+				}
+			})
+
+			t.Run("multiple", func(t *testing.T) {
+				out, err := exec.Command("./find-by-extended-attribute", "user.variety").CombinedOutput()
+				if err != nil {
+					t.Log(string(out))
+					t.Fatal(err)
+				}
+				got := string(out)
+				want := "test/artisan\ntest/youth\n"
+				if got != want {
+					t.Fatalf("Want: %s, Got: %s", want, got)
+				}
+			})
+		})
+
+		t.Run("xattrNameAndValue", func(t *testing.T) {
+			t.Run("single", func(t *testing.T) {
+				out, err := exec.Command("./find-by-extended-attribute", "user.mum", "climate").CombinedOutput()
+				if err != nil {
+					t.Log(string(out))
+					t.Fatal(err)
+				}
+				got := string(out)
+				want := "test/activity\n"
+				if got != want {
+					t.Fatalf("Want: %s, Got: %s", want, got)
+				}
+			})
+
+			t.Run("multiple", func(t *testing.T) {
+				out, err := exec.Command("./find-by-extended-attribute", "user.health", "security").CombinedOutput()
+				if err != nil {
+					t.Log(string(out))
+					t.Fatal(err)
+				}
+				got := string(out)
+				want := "test/policy\ntest/event\n"
+				if got != want {
+					t.Fatalf("Want: %s, Got: %s", want, got)
+				}
+			})
+		})
 	})
 }
